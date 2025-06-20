c47da9a4cc8800400d43909a2add2086fcc589ce
Shiv Shankar Menon Palat <shiv@pobox.com>
2025-06-20 07:50:10 -0400

    initial histo imp + add image

--- src/api.zig
+++ src/api.zig
@@ -1,4 +1,5 @@
 const Image = @import("image.zig").Image;
+const Histogram = @import("histogram.zig").Histogram;
 
 export fn quantizr_create_image_rgba(data: [*]const u8, width: i32, height: i32) ?*Image {
     const w: usize = @intCast(width);
@@ -11,4 +12,16 @@ export fn quantizr_create_image_rgba(data: [*]const u8, width: i32, height: i32)
 
 export fn quantizr_free_image(image: *Image) void {
     image.destroy();
+}
+
+export fn quantizr_create_histogram() ?*Histogram {
+    return Histogram.new() catch return null;
+}
+
+export fn quantizr_histogram_add_image(hist: *Histogram, image: *Image) void {
+    hist.add_image(image) catch return;
+}
+
+export fn quantizr_free_histogram(histogram: *Histogram) void {
+    histogram.destroy();
 }
---
+++ src/histogram.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const allocator = std.heap.c_allocator;
+
+const Image = @import("image.zig").Image;
+
+pub const HistogramEntry = struct {
+    color: [4]u8,
+    weight: u32,
+};
+
+pub const Histogram = struct {
+    map: std.AutoHashMap(u64, HistogramEntry),
+
+    pub fn new() !*Histogram {
+        const m = std.AutoHashMap(u64, HistogramEntry).init(allocator);
+        const h = try allocator.create(Histogram);
+        h.* = Histogram{
+            .map = m,
+        };
+        return h;
+    }
+
+    pub fn add_image(self: *Histogram, image: *const Image) anyerror!void {
+        const s = image.width * image.height;
+
+        var i: u32 = 0;
+        while (i < s * 4) : (i += 4) {
+            const pix = image.data[i .. i + 4];
+
+            var col = [4]u8{ 0, 0, 0, 0 };
+            if (pix[3] != 0) {
+                @memcpy(&col, pix);
+            }
+
+            const key: u64 = @as(u64, std.mem.readInt(u32, &col, .little));
+            if (self.map.getPtr(key)) |vp| {
+                vp.*.weight +|= 1;
+            } else {
+                try self.map.put(key, HistogramEntry{
+                    .color = col,
+                    .weight = 1,
+                });
+            }
+        }
+    }
+
+    pub fn destroy(self: *Histogram) void {
+        self.map.deinit();
+        allocator.destroy(self);
+    }
+};
